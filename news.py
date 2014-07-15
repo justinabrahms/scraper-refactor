c1c79850c9913581272fca4e0cd5f16e7d6ef2ff
Justin Abrahms <justin@abrah.ms>
2014-07-14 17:45:56 -0700

    Things now take lxml trees, not html text content.

--- news.py
+++ news.py
@@ -7,15 +7,13 @@ from pprint import pprint
 
 _URL = "http://jornaldeangola.sapo.ao"
 
-def get_image_top_news(source):
+def get_image_top_news(tree):
     """Retrieve the image of the news"""
-    tree = html.document_fromstring(source)
     return tree.xpath('//section[@class="top"]/figure/a/img/@src')
 
-def get_text_top_news(source):
+def get_text_top_news(tree):
     """ Retrieve the complete news url, the tittle and the first paragraph of the news."""
     
-    tree = html.document_fromstring(source)
     sec_wrapper = tree.xpath('//section[@class="top"]')
     for h in sec_wrapper:
         href_path = h.xpath('//section[@class="top"]/figure/a/@href')
@@ -24,15 +22,13 @@ def get_text_top_news(source):
             print "\n", full_url
             
             complete_page = urllib.urlopen(full_url).read()
-            get_complete_page(complete_page)
+            tree = html.document_fromstring(complete_page)
+            get_complete_page(tree)
 
 
-def get_complete_page(source):
-    tree_complete_page = html.document_fromstring(source)
-    titles = []
-
-    title = tree_complete_page.cssselect('h1.title-section2')[0].text_content()
-    news_snippet = tree_complete_page.xpath('//*[@id="main"]/section[1]/article/div[1]/div/p//text()')
+def get_complete_page(tree):
+    title = tree.cssselect('h1.title-section2')[0].text_content()
+    news_snippet = tree.xpath('//*[@id="main"]/section[1]/article/div[1]/div/p//text()')
 
     return (title, news_snippet)
 
@@ -40,7 +36,7 @@ def get_complete_page(source):
 class ImageTests(unittest.TestCase):
     def setUp(self):
         with open('./source.html') as f:
-            self.fixture = f.read()
+            self.fixture = html.document_fromstring(f.read())
 
     def test_returns_correct_value(self):
         url = ['http://thumbs.sapo.pt/?pic=http%3A%2F%2Fimgs.sapo.pt%2Fjornaldeangola%2Fimg%2Fthumb1%2F20140714065343pinda_simao_lgo.jpg&W=405&H=307&errorpic=http%3A%2F%2Fimgs.sapo.pt%2Fjornaldeangola2012%2Fimg%2Fdefaultja_051113.png']
@@ -49,22 +45,21 @@ class ImageTests(unittest.TestCase):
 class TextTests(unittest.TestCase):
     def setUp(self):
         with open('./detail.html') as f:
-            self.fixture = f.read()
+            self.fixture = html.document_fromstring(f.read())
 
     def test_complete_page(self):
         retval = (u'Investir nas lideran\xe7as d\xe1 qualidade ao ensino', 
                   [u'O ministro da Educa\xe7\xe3o esteve\xa0 no Lubango, Caluquembe e Caconda onde se  inteirou do funcionamento do sector. Pinda Sim\xe3o disse em entrevista ao', ' Jornal de Angola', u' que foi estabelecido um di\xe1logo com os directores de escolas e com o  Conselho de Ausculta\xe7\xe3o e Concerta\xe7\xe3o Social em busca de solu\xe7\xf5es para  as reivindica\xe7\xf5es apresentadas pelos professores.'])
         self.assertEqual(retval, get_complete_page(self.fixture))
 
-
-
-
     
 def main():
     source = urllib.urlopen(_URL).read()
-    for img in get_image_top_news(source):
+    tree = html.document_fromstring(source)
+
+    for img in get_image_top_news(tree):
         print img
-    title, snippets = get_text_top_news(source)
+    title, snippets = get_text_top_news(tree)
     print "\n", title
     for snippet in snippets:
         print "\n", snippet
